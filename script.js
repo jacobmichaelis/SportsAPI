c8e8f360f5a084b2bdb2d17200e976e97d3a8310
Art Macias <art.maciasjr@gmail.com>
2018-02-15 18:20:11 -0700

    Finished

--- script.js
+++ script.js
@@ -5,22 +5,29 @@ $(function(){
     var food = $("#foodInput").val();
 
     var myUrl = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=true&ingredients=" + food + "&limitLicense=false&number=3&ranking=1";
-    $.ajax(function() {
+    $.ajax({
       url: myUrl,
-      headers: {"X-Mashape-Key": "V4pZxYFBj6mshOk96fvs5m0NiIxVp1eBKPVjsnX6slS0dIjFVb",
-                "Accept": "application/json"
+      headers: {
+        'X-Mashape-Key': 'V4pZxYFBj6mshOk96fvs5m0NiIxVp1eBKPVjsnX6slS0dIjFVb',
+        'Accept': 'application/json'
       },
       type: "GET",
       dataType: "json",
-      success: getRecipeUrl
+      success: getRecipeUrl,
+      error: function(e) {
+        console.log(e);
+        $("#recipeHeader").html("Invalid request");
+      }
     });
   });
 });
 
 function getRecipeUrl(data) {
-	var length = data.length;
-	for(var i = 0; i < length; i++){
-		var id = d[i]["id"];
+  $("#recipeHeader").html("Your recipes:");
+  $("#foodOutput").html("");
+	$.each(data, function(index, obj){
+		var id = obj.id;
+    console.log(id);
 		$.ajax({
 			url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/' + id + '/information?includeNutrition=false',
 			headers: {
@@ -28,14 +35,18 @@ function getRecipeUrl(data) {
 				'Accept':'application/json'
 			},
 			type: 'GET',
-			dataType: "json"
-		}).done(function(data){
-			if (myload) {
-				addToCarousel(data);
-			}
-			else {
-				searchResult(data);
-			}
+			dataType: "json",
+      success: output,
+      error: function(e) {
+        console.log(e);
+        $("#recipeHeader").html("Invalid request");
+      }
 		});
-	}
+	});
+}
+
+function output(data) {
+  console.log(data);
+  var recipe = data.sourceUrl;
+  $("#foodOutput").append("<li><a target='_blank' href='" + recipe + "'>" + recipe + "</a></li>");
 }
