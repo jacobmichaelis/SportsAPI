a489695e14a393e207532fa54eb03ebb43486721
Art Macias <art.maciasjr@gmail.com>
2018-02-15 17:46:39 -0700

    changed to food api

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 $(function(){
-  $("#yodaSubmit").click(function(e) {
+  $("#foodSubmit").click(function(e) {
     e.preventDefault();
 
-    var str = $("#yodaInput").val().replace(" ", "+");
+    var food = $("#foodInput").val();
 
-    var yodaUrl = "https://yoda.p.mashape.com/yoda?sentence=" + str;
+    var myUrl = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=true&ingredients=" + food + "&limitLicense=false&number=3&ranking=1";
     $.ajax(function() {
-      url: yodaUrl,
-      X-Mashape-Key: "ctpYhtpDZvmshRldCNTlVt4l0JY5p12MwtFjsnplrj5I4BGO78",
-      Accept: "text/plain"
+      url: myUrl,
+      headers: {"X-Mashape-Key": "V4pZxYFBj6mshOk96fvs5m0NiIxVp1eBKPVjsnX6slS0dIjFVb",
+                "Accept": "application/json"
+      },
+      type: "GET",
+      dataType: "json",
+      success: getRecipeUrl
     });
   });
 });
+
+function getRecipeUrl(data) {
+	var length = data.length;
+	for(var i = 0; i < length; i++){
+		var id = d[i]["id"];
+		$.ajax({
+			url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/' + id + '/information?includeNutrition=false',
+			headers: {
+				'X-Mashape-Key':'V4pZxYFBj6mshOk96fvs5m0NiIxVp1eBKPVjsnX6slS0dIjFVb',
+				'Accept':'application/json'
+			},
+			type: 'GET',
+			dataType: "json"
+		}).done(function(data){
+			if (myload) {
+				addToCarousel(data);
+			}
+			else {
+				searchResult(data);
+			}
+		});
+	}
+}
